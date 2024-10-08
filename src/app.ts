61240a9404f941f92b2d4a28d81937365dfc2069
Adrián S <adriansotes@gmail.com>
2024-10-08 13:54:09 +0200

    fix: ejs index renders again

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import dotenv from "dotenv";
+import fs from "fs";
+import { fileURLToPath } from "node:url";
 import { connectMongoose } from "./lib/connectMongoose.js";
 
 import indexRouter from "./routes/index.js";
@@ -15,8 +17,9 @@ import developer from "./routes/developerJobRoutes.js";
 const app = express();
 
 // Get __dirname
-const __filename = new URL(import.meta.url).pathname;
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+console.log("Esto es __dirname: ", __dirname);
 
 // Load env variables from .env
 dotenv.config();
@@ -25,7 +28,19 @@ dotenv.config();
 connectMongoose();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+const viewsPath = path.join(__dirname, "views");
+console.log("Path to views:", viewsPath);
+
+fs.readdir(viewsPath, (err, files) => {
+  if (err) {
+    console.error("Error reading views directory:", err);
+  } else {
+    console.log("Files in views directory:", files);
+  }
+});
+
+app.set("views", viewsPath);
+
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
@@ -38,7 +53,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api", indexRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/developer-jobs", developer);
-console.log("Esto es developerJobREouter: ", developer);
 
 // catch 404 and forward to error handler
 app.use(function (req: Request, res: Response, next: NextFunction) {
