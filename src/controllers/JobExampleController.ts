fdf35db64112768e8b04078e3b13cf977cfc14fc
Adrián S <adriansotes@gmail.com>
2024-11-25 08:10:45 +0100

    feat: jobExample controller sends validation errors for creation and update

--- src/controllers/JobExampleController.ts
+++ src/controllers/JobExampleController.ts
@@ -64,27 +64,43 @@ class JobExampleController extends BaseController {
         !customer ||
         !category
       ) {
-        throw createCustomError(res.__("required_fields_for_job"));
+        throw createValidationError(res.__("validation_error"), [
+          res.__("required_fields_for_job"),
+        ]);
       }
 
       if (!/^\d{4}\/(0[1-9]|1[0-2])$/.test(launchPeriod)) {
-        throw createCustomError(
+        /* throw createCustomError(
           res.__("invalid_format_launch_period", { launchPeriod })
-        );
+        ); */
+
+        throw createValidationError(res.__("validation_error"), [
+          res.__("invalid_format_launch_period", { launchPeriod }),
+        ]);
       }
 
       if (linkToUrl && !isValidUrl(linkToUrl)) {
-        throw createCustomError(res.__("invalid_url_format", { linkToUrl }));
+        //throw createCustomError(res.__("invalid_url_format", { linkToUrl }));
+        throw createValidationError(res.__("validation_error"), [
+          res.__("invalid_url_format", { linkToUrl }),
+        ]);
       }
 
       if (linkToGitHub && !isValidUrl(linkToGitHub)) {
-        throw createCustomError(
+        /* throw createCustomError(
           res.__("invalid_git_url_format", { linkToGitHub })
-        );
+        ); */
+
+        throw createValidationError(res.__("validation_error"), [
+          res.__("invalid_git_url_format", { linkToGitHub }),
+        ]);
       }
 
       if (category && !isValidCategory(category, VALID_CATEGORIES)) {
-        throw createCustomError(res.__("invalid_category", { category }));
+        //throw createCustomError(res.__("invalid_category", { category }));
+        throw createValidationError(res.__("validation_error"), [
+          res.__("invalid_category", { category }),
+        ]);
       }
 
       const newJobExampleData = {
@@ -470,15 +486,21 @@ class JobExampleController extends BaseController {
       }
 
       if (linkToUrl && !isValidUrl(linkToUrl)) {
-        throw createCustomError(res.__("invalid_url_format", { linkToUrl }));
+        throw createValidationError(res.__("validation_error"), [
+          res.__("invalid_url_format", { linkToUrl }),
+        ]);
       }
 
       if (linkToGitHub && !isValidUrl(linkToGitHub)) {
-        throw createCustomError(res.__("invalid_url_format", { linkToGitHub }));
+        throw createValidationError(res.__("validation_error"), [
+          res.__("invalid_git_url_format", { linkToGitHub }),
+        ]);
       }
 
       if (category && !isValidCategory(category, VALID_CATEGORIES)) {
-        throw createCustomError(res.__("invalid_category", { category }));
+        throw createValidationError(res.__("validation_error"), [
+          res.__("invalid_category", { category }),
+        ]);
       }
 
       if (category && category.length === 0) {
